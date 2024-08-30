55d108cfbd34fb7a8343b15f062759551c74b507
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-08-30 20:26:41 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,13 @@
+let obj={
+    name:"vivek",
+    age:22,
+    weight:79,
+    location:"vizag",
+    occupation:"fresh student",
+    greet:function(){
+        console.log("hello ji kaise ho saare");
+    }
+};
+obj.greet();
+console.log(obj)
+console.log(typeof(obj));
