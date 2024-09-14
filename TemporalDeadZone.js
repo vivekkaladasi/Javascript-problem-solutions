5bd11e518b5822135a66f5411f6796bd127d8a87
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-14 17:07:50 +0530

    Create TemporalDeadZone.js

---
+++ TemporalDeadZone.js
@@ -0,0 +1,37 @@
+variable scoping
+//global scope
+//function scoping
+//block scoping
+
+
+//var age=15;//global scope
+
+console.log(age);
+
+//if(true){
+  //console.log(age)
+//}
+
+//for(let i=0; i<2;i++){
+  //console.log(age);
+//}
+
+//function sayHello(){
+  //console.log("hi",age)
+//}
+//sayHello();
+
+
+//function sayHello(){
+  //var name ="earth";
+  //console.log("hello",name);
+//}
+//sayHello();
+
+//block scope
+
+// temporal dead zone
+console.log(marks);
+
+let marks =100;
+
