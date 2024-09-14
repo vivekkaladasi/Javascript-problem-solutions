49f85a354e1b7742b760605e01d283c2656a1f34
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-14 18:54:50 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,17 @@
+//1 math object 
+console.log("vivek");
+
+console.log(Math.PI);
+
+console.log(Math.max(54,2,5,675,35,7535,3453,0));
+console.log(Math.min(54,2,5,675,35,7535,3453,0));
+
+console.log(Math.round(1.4));
+console.log(Math.round(1.6));
+console.log(Math.floor(2.2));
+
+console.log(Math.ceil(3.1));
+
+console.log(Math.abs(-5));
+
+console.log(Math.random());
