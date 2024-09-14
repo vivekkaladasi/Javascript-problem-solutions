adac78d0216feb4d1144299f5a9d6e5fb12b9cca
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-14 13:15:35 +0530

    Create hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,92 @@
+//console.log(age)
+// var age=25;
+// function hoisting
+
+
+
+
+
+ //let SayMyname=function (){
+  //console.log("jell");
+//}
+//SayMyname();  
+
+
+//class human{
+
+//}
+//const object1 = new human();
+
+//Function call stack
+//function is a first class citizen in js
+
+
+//let greet = function(){
+  //console.log("hello this is a greet funciton")
+//}
+
+//greet();
+
+
+
+//function greetme(greet, fullname){
+  //console.log("hello",fullname);
+  //greet();
+//}
+//function greet(){
+  //console.log("this is a greet function but in other way")
+
+//}
+
+
+//greetme(greet,"viv")
+
+//function solve(number){
+
+  //return function (number){
+    //return number*number;
+  //}
+//}
+//let ans =  solve();
+//let finalans = ans(10);
+
+
+
+
+//console.log(finalans);
+
+
+//const arr=[
+  //function (a,b){
+    //return a+b;
+  //},
+  //function (a,b){
+    //return a-b;
+  //},
+  //function (a,b){
+    //return a*b;
+  //}
+//];
+//let first = arr[2];
+//let ans = first(5,10);
+//console.log(ans);
+
+//let obj = {
+  //age:25,
+  //weight:22,
+  //height:122,
+  //greet(){
+  //  console.log("hello")
+  //}
+//}
+
+
+//console.log(obj.age);
+//obj.greet();
+
+//fucntion expression
+//console.log(greet)
+//var greet= function(){
+  //console.log("hello duniya")
+
+//}
