17f660baecd535dc4907aba524df367afccee544
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-18 12:42:19 +0530

    Create GarbageCollector.js

---
+++ GarbageCollector.js
@@ -0,0 +1,4 @@
+  //Garbage collector
+  //Garbage collection is an automatic memory management process that identifies and deallocates memory occupied by objects that are no longer needed by the program. In JavaScript, this process is performed by the runtime environment, typically a web browser or Node.js, rather than the programmer manually freeing memory.
+
+//So, the best part of using JavaScript is that you don't have to worry about the deallocation of unused memory manually. JavaScript handles it on its own, and this is one of the coolest features of JavaScript.
